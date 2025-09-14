34b1c5f3a5d753a526a822394a4a9f4c21c6a0e9
maulanarifaidoc <blogkumen45@gmail.com>
2025-09-14 12:54:23 +0700

    Add basic JavaScript documentation and implement various foundational concepts including conditionals, operators, data types, variables, and scope

--- rekursi/fibonacci.js
+++ rekursi/fibonacci.js
@@ -1,11 +1,12 @@
 //membuat proses perhitungan fibonacci
 //function dengan input nilai
 function fibonacci(n) {
-    //proses rekursi
+    // base case (kondisi berhenti)
     if (n === 0) return 0;
     if (n === 1) return 1;
-    return fibonacci(n - 1) + fibonacci(n - 2);
-} 
 
+    // recursive case (fungsi memanggil dirinya sendiri)
+    return fibonacci(n - 1) + fibonacci(n - 2);
+}
 //output
-console.log(fibonacci(6))
+console.log(fibonacci(6)); // Output: 8
